3ba2be46201e97c07dc3c11b09e563f8818a9576
CristianPumaES6 <cristian.puma.es6@gmail.com>
2020-04-22 23:37:01 -0500

    Se genero un nuevo componente report1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Reporte1Component } from './reporte1/reporte1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'report1', component: Reporte1Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Reporte1Component } from './reporte1/reporte1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Reporte1Component
   ],
   imports: [
     BrowserModule,
