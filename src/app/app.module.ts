90d657ee998b64d73f32d7113ff712ba2c6bce27
CristianPumaES6 <cristian.puma.es6@gmail.com>
2020-04-26 03:15:53 -0500

    ng g component report2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Reporte1Component } from './reporte1/reporte1.component';
+import { Report2Component } from './report2/report2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Reporte1Component
+    Reporte1Component,
+    Report2Component
   ],
   imports: [
     BrowserModule,
