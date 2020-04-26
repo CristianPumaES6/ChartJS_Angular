547baf94edc4cf0cf0f0fc61f72ae8caaffbc164
CristianPumaES6 <cristian.puma.es6@gmail.com>
2020-04-26 03:20:50 -0500

    Se esta agregando el componente al routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Reporte1Component } from './reporte1/reporte1.component';
+import { Report2Component } from './report2/report2.component';
 
 const routes: Routes = [
   { path: 'report1', component: Reporte1Component },
+  { path: 'report2', component: Report2Component },
 ];
 
 @NgModule({
